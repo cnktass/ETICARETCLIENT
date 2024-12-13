c7cbed74ee390c06232ffbc5066c786f4d3436a7
Cenk Aktas <cnktass@gmail.com>
2024-12-13 14:10:54 +0300

    Admin UI Bolumlerinin Yapilmasi Ve Component Module Routing Islemleri

---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerModule } from './customer/customer.module';
+import { ProductsModule } from './products/products.module';
+import { OrderModule } from './order/order.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustomerModule,
+    ProductsModule,
+    OrderModule,
+    CustomerModule,
+    DashboardModule
+  ]
+})
+export class ComponentsModule { }
+ 
